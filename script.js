e69adf3f1f5321e4e71deb64848094a9bac081e1
YTCount <166742934+ytcount@users.noreply.github.com>
2024-04-14 00:35:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -106,7 +106,7 @@ var sn = setInterval(function(){
 	
 	XMLHTTP.open("GET" ,"https://api.socialcounts.org/youtube-live-subscriber-count/"+channel, true);
 	XMLHTTP.send()
-}, 1000
+}, 1500
 )}
 
 function search() {
