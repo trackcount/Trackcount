e550bc39dc17caec2c1fc1297192c04e051f38f7
YTCount <166742934+ytcount@users.noreply.github.com>
2024-04-14 00:50:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const form = document.querySelector('form');
 form.addEventListener('keypress', function (event) {
   if (event.keyCode === 13) {
     event.preventDefault();
-    form.submit();
+    search();
   }
 });
 
